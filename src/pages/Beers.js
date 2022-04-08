8f60f5557d6b109b021454553dcc561181771caa
Marco Zampini <marco@bloom-it.io>
2022-04-08 22:53:04 +0200

    Iteration 3 - Part 2

--- src/pages/Beers.js
+++ src/pages/Beers.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Beers.css'
 
 const Beers = (props) => {
   return (
     <>
       <h1>Beers</h1>
-      <ul>
+      <ul className="beers">
         {props.beers.map((beer) => (
-          <li>
-            {beer.image} - {beer.name} - {beer.tagline} - Created by:{' '}
-            {beer.contributed_by}
+          <li className="beer" key={beer._id}>
+            <picture>
+              <img src={beer.image_url} alt={beer.name} />
+            </picture>
+            <div className="beer-info">
+              <h2>
+                <Link to={'/beers/' + beer._id}>{beer.name}</Link>
+              </h2>
+              <p className="tagline">{beer.tagline}</p>
+              <p className="created-by">Created by: {beer.contributed_by}</p>
+            </div>
           </li>
         ))}
       </ul>
